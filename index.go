7d4d37c26d1eb6d192a30e04e23086cce09d15ec
lufeijun1234 <lufeijun_1234@126.com>
2021-09-29 16:22:01 +0800

    调整一下忽略文件

--- index.go
+++ index.go
@@ -14,12 +14,13 @@ func init() {
 func main() {
 
 	// 1.创建路由
-	r := gin.Default()
+	// r := gin.Default()
+	r := gin.New()
 
 	routers.LoadApi(r)
 
 	// 3.监听端口，默认在8080
-	err := r.Run(":9009")
+	err := r.Run(":8008")
 
 	if err != nil {
 		fmt.Println("启动失败：err=%v\n", err)
